e43a26e0adb05717f4a5311b4e4f41af367b9542
Leon Seidel <leon.seidel@fau.de>
2025-01-18 22:31:03 +0100

    Fix packaging

--- vlm_drive/simulation/__init__.py
+++ vlm_drive/simulation/__init__.py
@@ -1,4 +1,9 @@
-from .agents import *
 from .carla_sim import CarlaSim
 from .vehicle import Vehicle
 from .waypoint_handler import WaypointHandler
+
+__all__ = [
+    "CarlaSim",
+    "Vehicle", 
+    "WaypointHandler"
+]
